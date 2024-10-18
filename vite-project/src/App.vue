0fe0b16a24d9bf848f6505ceec7d868a38922bbc
ezsky333 <ezsky333@gmail.com>
2024-10-18 07:49:37 +0000

    add artplayer

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,30 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Artplayer @get-instance="getInstance" :option="option" :style="style" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import Artplayer from "./components/Artplayer.vue";
+import artplayerPluginLibass from 'artplayer-plugin-libass'
+export default {
+  data() {
+    return {
+      option: {
+        container: '.artplayer-app',
+        url: "https://emall.bydauto.com.cn/uploads/ueditor/1683508816350440.mp4",
+        fullscreenWeb: true,
+        subtitle: {
+          url: 'https://artplayer.org/assets/sample/style-test.ass',
+        },
+        plugins: [
+          artplayerPluginLibass({
+            debug: true,
+            workerUrl: './assets/subtitles-octopus-worker.js',
+            wasmUrl: './assets/subtitles-octopus-worker.wasm',
+            fallbackFont: './assets/SourceHanSansCN-Bold.woff2'
+          }),
+        ],
+      },
+      style: {
+        width: "600px",
+        height: "400px",
+        margin: "60px auto 0",
+      },
+    };
+  },
+  components: {
+    Artplayer,
+  },
+  methods: {
+    getInstance(art) {
+      console.info(art);
+    },
+  },
+};
+</script>
